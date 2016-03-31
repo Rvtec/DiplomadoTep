3ae252785ed6b83e637d28075037281bfa2de01a
Rvtec <rvtec25@gmail.com>
2016-03-31 17:10:50 -0400

    Agregando paquete MiColeccion

---
+++ Practica2/src/MiColeccion/MiColeccion.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MiColeccion;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author rvargas
+ */
+public class MiColeccion implements java.util.List {
+
+    Object datos[];
+    boolean agregar;
+    int tamaño;
+    
+    public MiColeccion(){
+        
+     datos= new Object[3];
+     
+    
+    }
+    
+    public MiColeccion(int tamaño){
+        
+    datos= new Object[tamaño];
+   
+    
+    }
+    
+    @Override
+    public int size() {
+        
+        for(int i = 0; i < datos.length; i++){
+            if(datos[i]!= null){
+                tamaño++;
+                
+            }
+        }
+        return tamaño;
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean add(Object e) {
+        
+       for(int i = 0; i < datos.length; i++){
+           
+        if(datos[i] == null){
+            datos[i] = e;
+            return true;
+        }
+        
+       }
+       return false;
+    }
+ 
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  
+}
---
+++ Practica2/src/MiColeccion/PruebaColecciones.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MiColeccion;
+
+import Edificio.Edificio;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author rvargas
+ */
+public class PruebaColecciones {
+    
+    public static void main (String [] args){
+    
+    MiColeccion miCol1 = new MiColeccion();
+    MiColeccion miCol2 = new MiColeccion(5);
+    ArrayList<String> listaArr = new ArrayList<String>();
+    LinkedList linkList = new LinkedList();
+   
+    
+    miCol2.add(new Edificio(5,"Torre",54));
+    miCol2.add(new Edificio(6,"Torre",54));
+    miCol2.add(new Edificio(7,"Torre",54));
+    listaArr.add("Hola");
+    listaArr.add("Mundo");
+    listaArr.add("Java");
+    linkList.add(1);
+    linkList.add(2);
+    linkList.add(3);
+    
+       // System.out.println(miCol2.size());
+        
+        
+        
+        
+    
+    }
+    
+    
+    
+    public static void recorrerLista(){
+
+          
+        
+       }
+}
+
+
